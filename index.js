74c1a079512d79bb1e6d1aee5380037dc3d62992
Antonio Gleyser <gleyser@MacBook-Pro-de-Gleyser.local>
2024-01-18 16:21:59 -0300

    logica do contador e finalizacao do projeto

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-let decrease = document.getElementById('decreaseBtn');
-let reset = document.getElementById('resetBtn');
-let increase = document.getElementById('increaseBtn');
+//Contador programa
+//By Antonio Gleyser
+
+const decreaseBtn = document.getElementById('decreaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+const countLabel = document.getElementById('countLabel');
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count ++;
+    countLabel.textContent = count;
+
+}
+
+decreaseBtn.onclick = function() {
+    count --;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+
+}
